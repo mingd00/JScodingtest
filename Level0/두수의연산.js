fc73b719520f6a0f2e136cdc59f4e8b7e8c9492d
mingd00 <rkdalswl403@naver.com>
2024-06-23 22:36:55 +0900

    [feat] 두 수의 연산
    - 합, 차, 곱, 몫

---
+++ Level0/두수의연산.js
@@ -0,0 +1,10 @@
+const plus = (num1, num2) => num1 + num2
+const minus = (num1, num2) => num1 - num2
+const multi = (num1, num2) => num1 * num2
+const divide = (num1, num2) => Math.floor(num1 / num2)
+
+
+console.log(plus(2, 3))
+console.log(minus(2, 3))
+console.log(multi(2, 3))
+console.log(divide(2, 3))
