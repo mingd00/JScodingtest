c1cbbacb0735b6618d13a4cac7796d1f522a76d2
mingd00 <rkdalswl403@naver.com>
2024-06-23 21:24:15 +0900

    [feat] JS 코딩테스트 기초

---
+++ Basic/dfs.js
@@ -0,0 +1,16 @@
+const graph = {
+    1: [2, 3],
+    2: [4],
+    3: [4, 5],
+    4: [],
+    5: []
+}
+
+const dfs = (here, visited = new Set()) => {
+    if (visited.has(here)) return
+    visited.add(here)
+    console.log(here)
+    graph[here].forEach(e => dfs(e, visited));
+}
+
+dfs(1);
