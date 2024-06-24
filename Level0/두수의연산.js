99326fc840b8c5177c76a275a9942c07fffecf77
mingd00 <rkdalswl403@naver.com>
2024-06-24 09:08:05 +0900

    [feat] 두 수의 연산
    - 합, 차, 곱, 몫, 나머지

--- Level0/두수의연산.js
+++ Level0/두수의연산.js
@@ -1,10 +1,11 @@
 const plus = (num1, num2) => num1 + num2
 const minus = (num1, num2) => num1 - num2
 const multi = (num1, num2) => num1 * num2
-const divide = (num1, num2) => Math.floor(num1 / num2)
-
+const quotient = (num1, num2) => Math.floor(num1 / num2)
+const remainder = (num1, num2) => num1 % num2
 
 console.log(plus(2, 3))
 console.log(minus(2, 3))
 console.log(multi(2, 3))
-console.log(divide(2, 3))
+console.log(quotient(2, 3))
+console.log(remainder(2, 3))
